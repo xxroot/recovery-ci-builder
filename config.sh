ecac683acd2018d45d765b670eea062e2634df78
R E D F O X <95732139+FoxRedd@users.noreply.github.com>
2022-03-27 19:31:20 +0800

    begonia

--- config.sh
+++ config.sh
@@ -25,9 +25,9 @@ export CCACHE_SIZE="50G"
 export CCACHE_DIR="$HOME/work/.ccache"
 export J_VAL=16
 
-if [ ! -z "$PLATFORM" ]; then
-    export KERNEL_PATH="kernel/$OEM/$PLATFORM"
-else
-    export KERNEL_PATH="kernel/$OEM/$DEVICE"
-fi
+#if [ ! -z "$PLATFORM" ]; then
+#    export KERNEL_PATH="kernel/$OEM/$PLATFORM"
+#else
+#   export KERNEL_PATH="kernel/$OEM/$DEVICE"
+#fi
 export DT_PATH="device/$OEM/$DEVICE"
