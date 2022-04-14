9c040d2c50c4891f21764af746b31f7a78c97c0f
donsenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2022-04-14 23:44:43 +0800

    update device tree

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_10.0"
-export DT_LINK="https://github.com/mastersenpai0405/recovery_device_infinix_X683 -b android-11.0"
+export DT_LINK="https://github.com/mastersenpai0405/recovery_device_infinix_X683"
 
 export DEVICE="X683"
 export OEM="infinix"
